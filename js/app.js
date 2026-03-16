6133b8100e4b60c25fa167309d3651990585e46a
yaniortiz987@gmail.com <yaniortiz987@gmail.com>
2026-03-16 15:48:23 -0300

    Estructura + eventos basicos

--- js/app.js
+++ js/app.js
@@ -3,15 +3,32 @@ console.log(valor);
 const btnGuardar = document.querySelector("[data-name='btn-guardar']");
 const textoTarea = document.querySelector("input¨[name='nueva-tarea']");
 const listadoTareas = document.querySelector("ul");
+const btnLimpiar = document.querySelector("#btn-limpiar")
+const btnsEliminar = document.querySelector(".eliminar")
 
 btnGuardar.addEventListener("click", function(evento){
     console.log(textoTarea.value);
     const tarea = `<li class="flex justify-between items-center">
-            <span>Sacar al perro</span> 
-            <button class="bg-red-900 px-3 rounded-lg">x</button>
+            <span> ${textoTarea.value} </span> 
+            <button class="bg-red-900 px-3 rounded-lg" eliminar>x</button>
     </li>`
-    listadoTareas.insertAdjacentHTML("afterend", tarea)
-
-
+    listadoTareas.insertAdjacentHTML("beforeend", tarea)
 
 });
+
+btnLimpiar.addEventListener("click", function(evento){
+    listadoTareas.innerHTML = ""
+})
+
+console.log(btnsEliminar);
+
+btnsEliminar.forEach(
+    function(elemento){
+    elemento.addEventListener("click", function (evento) {
+        evento.target.closest("li").outerHTML = ""
+        })
+    }
+)
+
+
+
