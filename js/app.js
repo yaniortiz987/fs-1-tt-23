94d0ad3b8c1c30f036c926efb00c9f436f92238c
yaniortiz987@gmail.com <yaniortiz987@gmail.com>
2026-03-18 15:19:15 -0300

    Se agrega el localStorage y se implementa

--- js/app.js
+++ js/app.js
@@ -1,18 +1,25 @@
 console.log(valor);
 
 const btnGuardar = document.querySelector("[data-name='btn-guardar']");
-const textoTarea = document.querySelector("input¨[name='nueva-tarea']");
+const textoTarea = document.querySelector("input[name='nueva-tarea']");
 const listadoTareas = document.querySelector("ul");
-const btnLimpiar = document.querySelector("#btn-limpiar")
-const btnsEliminar = document.querySelector(".eliminar")
+const btnLimpiar = document.querySelector("#btn-Limpiar")
+let btnsEliminar = []
+
+const historicoTareas = JSON.parse(recuperar("estados")) ?? []
+
+
 
 btnGuardar.addEventListener("click", function(evento){
     console.log(textoTarea.value);
+    historicoTareas.push(textoTarea.value)
+
     const tarea = `<li class="flex justify-between items-center">
             <span> ${textoTarea.value} </span> 
-            <button class="bg-red-900 px-3 rounded-lg" eliminar>x</button>
+            <button class="bg-red-900 px-3 rounded-lg eliminar">x</button>
     </li>`
     listadoTareas.insertAdjacentHTML("beforeend", tarea)
+    guardar("estados", JSON.stringify(historicoTareas))
 
 });
 
@@ -20,15 +27,55 @@ btnLimpiar.addEventListener("click", function(evento){
     listadoTareas.innerHTML = ""
 })
 
-console.log(btnsEliminar);
+function recuperarTodo(){
+    const mostrar = historicoTareas.map(
+        function (elemento) {
+            return `<li class="flex justify-between items-center">
+            <span> ${elemento} </span> 
+            <button class="bg-red-900 px-3 rounded-lg eliminar">x</button>
+    </li>`
+        }
+    )
+console.log(mostrar);
 
-btnsEliminar.forEach(
+    listadoTareas.insertAdjacentHTML("beforeend", mostar.join(""))
+}
+
+
+listadoTareas.addEventListener("click", function(evento){
+    /*Agrego como delegacion de eventos
+    Llego al hijo atravez del padre*/
+
+    console.log("Evento:", evento.target);
+
+    if (evento.target.tagName == "BUTTON") {
+        evento.target.closest("li").remove()
+    }
+
+    if (evento.target.tagName == "SPAN") {
+        alert(evento.target.textContent)
+    }
+
+})
+
+recuperarTodo()
+
+
+/*setInterval(
+    function () {
+        btnsEliminar = document.querySelectorAll(".eliminar")
+
+        btnsEliminar.forEach(
     function(elemento){
-    elemento.addEventListener("click", function (evento) {
-        evento.target.closest("li").outerHTML = ""
+         elemento.addEventListener("click", function (evento) {
+             evento.target.closest("li").outerHTML = ""
         })
     }
 )
+    }
+    , 3000)*/
+
+
 
 
 
--- js/auxiliar.js
+++ js/auxiliar.js
@@ -1,1 +1,10 @@
 var valor = 10000
+
+function guardar(clave,valor) {
+localStorage.setItem(clave,valor)
+return true
+}
+
+function recuperar(clave) {
+    return localStorage.getItem(clave)
+}
